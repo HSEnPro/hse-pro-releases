10db6e2d51bef5cfdccf831d4e8225689c373e26
HSEnPro <abuyabes.you@gmail.com>
2026-06-10 05:46:15 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('hseUpdater', {
-  version: (function(){ try { return ipcRenderer.sendSync('app-version'); } catch(e){ return ''; } })(),
-  onAvailable: (cb) => ipcRenderer.on('update-available', (e, info) => cb(info)),
-  onDownloaded: (cb) => ipcRenderer.on('update-downloaded', (e, info) => cb(info)),
-  restart: () => ipcRenderer.send('update-restart')
-});
